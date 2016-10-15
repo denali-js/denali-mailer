347447f5721b8eb2cefec3837f7d6756bb3dfb77
Ilya Radchenko <ilya@burstcreations.com>
2016-10-14 22:37:53 -0400

    Add basic blueprint

---
+++ blueprints/mailer/files/app/mailers/__name__/mailer.js
@@ -0,0 +1,6 @@
+import { Mailer } from 'denali-mailer';
+
+export default class <%= className %>Mailer extends Mailer {
+  from = 'from@email.com';
+  to = 'to@email.com';
+}
---
+++ blueprints/mailer/index.js
@@ -0,0 +1,17 @@
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+import { Blueprint } from 'denali';
+
+export default class MailerBlueprint extends Blueprint {
+  static blueprintName = 'mailer';
+  static description = 'Generate a basic mailer to customize';
+
+  params = [ 'name' ];
+
+  locals({ name }) {
+    return {
+      name,
+      className: upperFirst(camelCase(name))
+    };
+  }
+}
