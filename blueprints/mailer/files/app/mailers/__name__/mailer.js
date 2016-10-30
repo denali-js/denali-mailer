55a809d1860428123bb0289a8d0ae66c4838bbc2
Dave Wasmer <davewasmer@gmail.com>
2016-10-29 23:16:24 -0600

    fix(blueprint): remove to: and add subject: to mailer blueprint

--- blueprints/mailer/files/app/mailers/__name__/mailer.js
+++ blueprints/mailer/files/app/mailers/__name__/mailer.js
@@ -1,6 +1,9 @@
 import { Mailer } from 'denali-mailer';
 
 export default class <%= className %>Mailer extends Mailer {
+
   from = 'from@email.com';
-  to = 'to@email.com';
+
+  subject = 'Hello world!';
+
 }
